48ba6a155648ce957036e53836af24d5f173fafe
Rohan Kulkarni <rohank2502@gmail.com>
2023-09-23 16:19:52 +0530

    added images

--- src/components/previous-events-section/PreviousEvents.jsx
+++ src/components/previous-events-section/PreviousEvents.jsx
@@ -1,21 +1,29 @@
-import "./styles.css";
 import IMG1 from "../../assets/compressed/events/1.webp";
 import IMG2 from "../../assets/compressed/events/2.webp";
 import IMG3 from "../../assets/compressed/events/3.webp";
 import IMG4 from "../../assets/compressed/events/4.webp";
 import IMG5 from "../../assets/compressed/events/5.webp";
 import IMG6 from "../../assets/compressed/events/6.webp";
+import Carousel from "./Carousel";
 
 const PreviousEvents = () => {
   return (
     <section className="mx-48 flex flex-col gap-24 lg:mx-24 lg:gap-4 sm:mx-8">
-      {/* <div className="flex flex-row gap-7 place-items-center ">
+      <div className="flex flex-row gap-7 place-items-center ">
         <p className="h-1 flex-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></p>
         <h2 className="text-6xl font-bold text-white lg:text-5xl sm:text-3xl">
           Previous Events
         </h2>
-      </div> */}
-      
+      </div>
+      <div className="flex flex-wrap  justify-evenly h-fit gap-12 ">
+        <img src={IMG1} className="h-72 rounded-2xl" />
+        <img src={IMG6} className="h-72 rounded-2xl" />
+        <img src={IMG2} className="h-72 rounded-2xl" />
+        <img src={IMG3} className="h-72 rounded-2xl" />
+        <img src={IMG4} className="h-72 rounded-2xl" />
+        <img src={IMG5} className="h-72 rounded-2xl" />
+      </div>
+
       <div className=" h-36 bg-black rounded-lg bg-opacity-30 backdrop-filter backdrop-blur-lgp-6 flex flex-row  justify-evenly place-items-center  lg:flex-wrap lg:h-full lg:p-8 sm:gap-4 p-8 gap-4">
         <p className="flex flex-row place-items-center gap-4 ">
           <span className="font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-indigo-700 via-purple-700 to-pink-700 sm:text-2xl">
