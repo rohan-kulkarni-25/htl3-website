b16b1487c3de00df8c41bbb6bcef7ba93148c5ad
Shivang Parmar <shivangparmar537@gmail.com>
2023-10-03 16:42:42 +0530

    made few changes

--- src/components/previousMeetups/PreviousMeetups.jsx
+++ src/components/previousMeetups/PreviousMeetups.jsx
@@ -17,12 +17,12 @@ const PreviousMeetups = () => {
 
     ];
     return (
-        <section className="mx-48 my-48 flex flex-col mt-24 gap-24 lg:mx-24 lg:gap-4 sm:mx-8 sm:mt-24">
+        <section className="mx-48 flex flex-col mt-24 gap-24 lg:mx-24 lg:gap-4 sm:mx-8 sm:mt-24">
             <div className="flex flex-row gap-7 place-items-center lg:mx-24 sm:mx-8">
                 <h2 className="text-4xl font-semibold font-poppins text-white lg:text-5xl sm:text-3xl">
                     City Meetup 2022
                 </h2>
-                <p className="h-1 flex-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></p>
+                <p className="h-0.5 rouded-lg flex-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></p>
             </div>
             <div className="flex flex-wrap justify-center items-start content-start gap-12">
                 {Meetups.map((meetup) => (
--- src/pages/meetups.js
+++ src/pages/meetups.js
@@ -4,6 +4,7 @@ const App = () => {
     return (
         <section>
             <PreviousMeetups />
+            <div className="h-48 lg:h-24 sm:h-20" />
         </section>
     );
 };
