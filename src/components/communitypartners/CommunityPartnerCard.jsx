c1ec48005896a1f267ef41aab90869d0ad6000e1
Rohan Kulkarni <rohank2502@gmail.com>
2024-04-16 16:02:57 +0530

    Updated Commnity Partners.

--- src/components/communitypartners/CommunityPartnerCard.jsx
+++ src/components/communitypartners/CommunityPartnerCard.jsx
@@ -1,6 +1,6 @@
 const CommunityPartnerCard = ({ url,name,socialprofile }) => {
   return (
-    <a target="_blank" rel="noreferrer" href={socialprofile} className="flex sm:gap-2 h-20 sm:mt-4 shadow-sm shadow-white  rounded-md ">
+    <a target="_blank" rel="noreferrer" href={socialprofile} className="flex sm:gap-2 h-20 sm:mt-4 sm:w-28 sm:h-fit shadow-sm shadow-white  rounded-md ">
       <img className="aspect-auto p-1 flex-1 " src={url} alt={name} />
     </a>
   );
