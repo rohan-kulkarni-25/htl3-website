b0904d151911bef479405a9ee35e3cdf4d6b51d5
Rohan Kulkarni <rohank2502@gmail.com>
2024-05-15 22:53:12 +0530

    Added Sponsor

--- src/pages/Sponsors.jsx
+++ src/pages/Sponsors.jsx
@@ -4,17 +4,17 @@ import SponsorCard from "../components/sponsors/SponsorCard";
 
 const Sponsors = () => {
   return (
-    <div className=" flex flex-col gap-8  mt-24  mx-24 sm:mx-4 sm:gap-12 overflow-y-scroll">
+    <div className=" flex flex-col gap-8  my-24  mx-24 sm:mx-4 sm:gap-12 overflow-y-scroll">
       <div className="flex flex-col gap-12 ">
         <div className="sm:hidden">
-        <PageHeading title={"[ POWERED BY ]"}  />
+          <PageHeading title={"[ POWERED BY ]"} />
         </div>
 
         <div className="bg-gradient-to-r sm:flex hidden  from-indigo-500 via-purple-500 to-pink-500 bg-clip-text  justify-center">
-            <p className=" font-mokoto text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 inline-block text-transparent bg-clip-text tracking-widest text-2xl">
-              WEB3 VC PARTNER
-            </p>
-          </div>
+          <p className=" font-mokoto text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 inline-block text-transparent bg-clip-text tracking-widest text-2xl">
+            WEB3 VC PARTNER
+          </p>
+        </div>
         <div className="flex justify-center items-center w-fit mx-auto py-8 px-12 rounded-md">
           <SponsorCard
             url="https://res.cloudinary.com/dsdjai01b/image/upload/v1715077791/Polygon_Primary_Light_rllkof.png"
@@ -53,9 +53,9 @@ const Sponsors = () => {
             />
           </div>
         </div>
-
       </div>
-      <div className="flex flex-col gap-12 w-1/3 sm:w-full  p-12 rounded-md mx-auto">
+      <div className="flex flex-row sm:flex-col sm:items-center justify-evenly gap-8">
+        <div className="flex flex-col gap-12 w-1/3 sm:w-full  p-12 rounded-md mx-auto">
           <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text flex justify-center">
             <p className=" font-mokoto text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 inline-block text-transparent bg-clip-text tracking-widest text-2xl">
               EDUCATION PARTNER
@@ -69,9 +69,20 @@ const Sponsors = () => {
             />
           </div>
         </div>
-
-      <div className="mx-auto">
-        <TBAComponent />
+        <div className="flex flex-col gap-12 w-1/3 sm:w-full  p-12 rounded-md mx-auto">
+          <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text flex justify-center">
+            <p className=" font-mokoto text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 inline-block text-transparent bg-clip-text tracking-widest text-2xl">
+              IMPACT PARTNER
+            </p>
+          </div>
+          <div className="flex justify-center items-center">
+            <SponsorCard
+              url="https://res.cloudinary.com/dsdjai01b/image/upload/v1715603016/DablClubLogo-White_owvhmx.png"
+              link="https://github.com/"
+              size={72}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
