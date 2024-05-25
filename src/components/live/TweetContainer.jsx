37f5c9922906dd8299a51a55a0b7475e7ddc373d
Rohan Kulkarni <rohank2502@gmail.com>
2024-05-25 16:31:40 +0530

    updated tweets

--- src/components/live/TweetContainer.jsx
+++ src/components/live/TweetContainer.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { TwitterTweetEmbed } from 'react-twitter-embed';
+import React from "react";
+import { Tweet } from "react-tweet";
+import { TwitterTweetEmbed } from "react-twitter-embed";
 
 const TweetsContainer = () => {
-
-    let tweets = ["1793680562198311412","1794054235169984704","1793754883473977796","1644641869429895168","1793979366323679710"];
+  let tweets = [
+    "1793680562198311412",
+    "1794054235169984704",
+    "1793754883473977796",
+    "1644641869429895168",
+    "1793979366323679710",
+  ];
 
   return (
     <div className="h-full  flex justify-center  overflow-hidden relative bg-countdown">
-      <div className="flex justify-center flex-col items-center w-full gap-8  animate-scroll">
-
-{
-    tweets.map((id)=>{
-        // return <TwitterTweetEmbed tweetId={id} key={id} />
-      return <TwitterTweetEmbed tweetId={id} key={id}  />
-})
-}
-        
-
+      <div className="flex  justify-center flex-col items-center w-full gap-3  animate-scroll">
+        {tweets.map((id) => {
+          return <div className="w-3/4" key={id}>
+            <Tweet id={id} />
+          </div>;
+        })}
       </div>
     </div>
   );
